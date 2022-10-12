ad4beba87ddd92768ca89cfaac57368121baf067
mk <pinch24@gmail.com>
2022-10-12 11:22:18 +0900

    02. Counter

--- 02. Svelte Code Lab/01. Week 1/first/my-svelte-project/src/App.svelte
+++ 02. Svelte Code Lab/01. Week 1/first/my-svelte-project/src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+	let count = 2;
+	function increase() {
+		count++;
+	}
+
+	let array = [1, 2, 3, 4];
+	function addItem() {
+		array.push(array.length + 1);
+		console.log(array);
+	}
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Simple Counter</h1>
+	<p>Count: { count }</p>
+	<button on:click={ increase }>Add count</button>
+	<p>List: { JSON.stringify(array) }</p>
+	<button on:click={ addItem }>Add Item to List</button>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
