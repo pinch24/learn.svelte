9763d61a43a54e20f91a76bb2db03fadb19082d9
mk <pinch24@gmail.com>
2022-10-14 12:09:13 +0900

    04. Custom Tab Bar

---
+++ 02. Svelte Code Lab/01. Week 1/caffe/src/pages/Home.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from '../components/Header.svelte';
+	import { navigate } from 'svelte-routing';
+
+	function goOrder() {
+		// do gtag
+		navigate('/order');
+	}
+</script>
+
+<Header/>
+
+<div class="container px-4">
+	<h1 class="text-4xl font-bold my-8">Caffe</h1>
+	<p class="mb-4">Online Coffee Order Service</p>
+	<button class="btn btn-outline-primary" on:click={ goOrder }>Now Order</button>
+</div>
