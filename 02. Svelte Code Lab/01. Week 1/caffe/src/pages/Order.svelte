2336fd5e57dbe3793f8b993d64846fb70248f923
mk <pinch24@gmail.com>
2022-10-15 22:17:44 +0900

    03. Caffe - 2

--- 02. Svelte Code Lab/01. Week 1/caffe/src/pages/Order.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Header from '../components/Header.svelte';
-</script>
-
-<Header/>
-
-<div class="container">
-	<h1>Order</h1>
-</div>
---
+++ 02. Svelte Code Lab/01. Week 1/caffee/src/pages/Order.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header/>
+
+<div class="container px-4">
+	<h1 class="text-4xl font-bold mb-8">Order</h1>
+
+	<dl>
+		<dt>Espresso</dt>
+		<dd>
+			2,800
+			<button class="btn btn-sm btn-outline-secondary">Select</button>
+		</dd>
+		<dt>Americano</dt>
+		<dd>
+			3,000
+			<button class="btn btn-sm btn-outline-secondary">Select</button>
+		</dd>
+	</dl>
+
+	<hr class="my-4">
+
+	<h2 class="text-2xl font-bold mb-2">Order Sheet</h2>
+
+	Total: 0
+</div>
