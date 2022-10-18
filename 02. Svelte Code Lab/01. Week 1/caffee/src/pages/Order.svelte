928d5edafbdbdf55a3529b869fd791036c5ebaed
mk <pinch24@gmail.com>
2022-10-18 10:24:03 +0900

    03. Caffe - 3

--- 02. Svelte Code Lab/01. Week 1/caffee/src/pages/Order.svelte
+++ 02. Svelte Code Lab/01. Week 1/caffee/src/pages/Order.svelte
@@ -1,5 +1,34 @@
 <script>
+    import { element } from 'svelte/internal';
+    import About from '../pages/About.svelte';
 	import Header from '../components/Header.svelte';
+
+	const formatter = Intl.NumberFormat('ko-KR');
+
+	const menu = [
+		{ name: 'Esprosso', price: 2800 },
+		{ name: 'Americano', price: 3000 },
+		{ name: 'Cafe Latte', price: 3400 },
+		{ name: 'Vanilla Latte', price: 3700 },
+	]
+
+	let selected = [];
+
+	function toggleItem(name) {
+		if (selected.find(element => element.name === name)) {
+			selected = selected.filter(element => element.name !== name);
+		}
+		else {
+			const item = menu.find(element => element.name === name);
+			selected = [...selected, item];
+		}
+	}
+
+	let sum = 0;
+	$: {
+		sum = 0;
+		selected.forEach(item => sum += item.price);
+	}
 </script>
 
 <Header/>
@@ -8,21 +37,26 @@
 	<h1 class="text-4xl font-bold mb-8">Order</h1>
 
 	<dl>
-		<dt>Espresso</dt>
-		<dd>
-			2,800
-			<button class="btn btn-sm btn-outline-secondary">Select</button>
-		</dd>
-		<dt>Americano</dt>
-		<dd>
-			3,000
-			<button class="btn btn-sm btn-outline-secondary">Select</button>
-		</dd>
+		{ #each menu as item }
+			<dt>{ item.name }</dt>
+			<dd>
+				{ formatter.format(item.price) }
+				<button class="btn btn-sm btn-outline-primary" on:click={ () => toggleItem(item.name) }>
+					{ selected.find(element => element.name === item.name) ? 'Selected' : 'Select' }
+				</button>
+			</dd>
+		{ /each }
 	</dl>
 
 	<hr class="my-4">
 
 	<h2 class="text-2xl font-bold mb-2">Order Sheet</h2>
 
-	Total: 0
+	<ul class="mb-2">
+		{ #each selected as item }
+			<li>- { item.name} </li>
+		{ /each }
+	</ul>
+
+	<p>Sum: {formatter.format(sum)}</p>
 </div>
