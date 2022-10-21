757e45abcb1069f38af8e67427ca324ba2832496
mk <pinch24@gmail.com>
2022-10-21 12:06:59 +0900

    3. Svelte Component - 1

---
+++ 03. Svelte and Sapper in Action/eval/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	let status = 200;
+	let message = "This is a message.";
+
+	$: error = status >= 200;
+</script>
+
+<main>
+	<label>
+		Status <input type="number" bind:value={ status }>
+	</label>
+
+	<div class:error>{ message }</div>
+</main>
+
+<style>
+	.error {
+		color: red;
+		font-weight: bold;
+	}
+</style>
